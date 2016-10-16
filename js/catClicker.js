d060606175759b0ab6ad5c812055149ab476be24
Lin Zhang <lin.zhang.ux@gmail.com>
2016-10-16 17:01:56 -0600

    Update cat counter via view only

--- js/catClicker.js
+++ js/catClicker.js
@@ -9,12 +9,54 @@ $(document).ready(function(){
     $('#cat-click-counter').text(counter);
   });*/
 
+
+  var cats = [];
   var catOne = new catClicker("Kitty","images/Jeff-the-Cat.jpg", "catOne");
   var catTwo = new catClicker("Kat","images/Jeff-the-Cat.jpg", "catTwo");
   var catThree = new catClicker("Ken","images/Jeff-the-Cat.jpg", "catThree");
+  cats.push(catOne, catTwo, catThree);
+  for(var i = 0; i < cats.length; i++){
+      if(!cats[i].isShow){
+        cats[i].display();
+      }
+      $('#'+cats[i].catId+"pic").click(function(){
+        var it = $(this).attr('id');
+        var id = it.substring(0, it.length-3);
+        var c = parseInt($('#'+ id).text()) + 1;
+        $('#'+ id).text(c);
+
+      });
+    // var catItem = document.createElement('li');
+    // catItem.textContent = cats[i].catName;
+    // $("#catNames").append("<li>"+cats[i].catName+"</li>");
+    // catItem.addEventListener('click',(function(catId){
+    //   var thisCat = cats[i];
+    //   if(!thisCat.isShow){
+    //     thisCat.display();
+    //   }
+    // })(cats[i]));
+    // $('#catNames').append(catItem);
+  }
+
+  // var catTwo = new catClicker("Kat","images/Jeff-the-Cat.jpg", "catTwo");
+  // var catThree = new catClicker("Ken","images/Jeff-the-Cat.jpg", "catThree");
+  // cats.push(catOne, catTwo, catThree);
+  // cats.push(catOne);
+  // for(var i = 0; i < cats.length; i++){
+  //   var catItem = document.createElement('li');
+  //   catItem.textContent = cats[i].catName;
+  //   $("#catNames").append("<li>"+cats[i].catName+"</li>");
+  //   catItem.addEventListener('click',(function(catId){
+  //     var thisCat = cats[i];
+  //     if(!thisCat.isShow){
+  //       thisCat.display();
+  //     }
+  //   })(cats[i]));
+  //   $('#catNames').append(catItem);
+  // }
 
   // clear the screen for testing
-  document.body.innerHTML = '';
+/*  document.body.innerHTML = '';
   document.body.style.background="white";
   var nums = [1,2,3]; // Let's loop over the numbers in our array
   for (var i = 0; i < nums.length; i++) {
@@ -28,17 +70,21 @@ $(document).ready(function(){
       alert(elem.innerText);
     });*/
 
-    //We immediately invoke it by wrapping it in parentheses and calling it right away, passing in num.
+    /*//We immediately invoke it by wrapping it in parentheses and calling it right away, passing in num.
     //This method of wrapping an anonymous function in parentheses and calling it right away is
     //called an IIFE (Immediately-Invoked Function Expression, pronounced like "iffy")
     elem.addEventListener('click', (function(numCopy){
+      //The outer function returns the inner function to the event listener.
+      //Because of the way JavaScript scope works, that inner function has access to numCopy.
+      //In the near future, num will increment, but that doesn't matter.
+      //The inner function has access to numCopy, which will never change.
       return function(){
         alert(numCopy)
       };
     })(num));
       // finally, let's add this element to the document
     document.body.appendChild(elem);
-  };
+  };*/
 
 });
 
@@ -47,24 +93,29 @@ var catClicker = function(name, picture, id){
   this.picture = picture;
   this.catId = id;
   this.counter = 0;
-  this.init();
+  //this.init();
+  this.isShow = false;
 }
 catClicker.prototype.init = function (){
-  this.display();
+  //this.display();
   this.addCatClicker();
 }
 catClicker.prototype.display = function(){
   $('#container').append("<div class=\"cat-name\">"+this.catName+"</div>");
   $('#container').append("<div class=\"cat-pic\" id=\""+this.catId+"pic"+"\"><img src='"+this.picture+"'/></div>");
   $('#container').append("<div class=\"cat-click-counter\" id=\""+this.catId+"\">"+this.counter+"</div>");
+  this.show = true;
 };
 catClicker.prototype.addCatClicker = function(){
   var catPic = "#"+this.catId+"pic";
-  var counter = this.counter;
-  var catId = this.catId;
+  //var counter = this.counter;
+  //var catId = this.catId;
+  var self = this;
   $(catPic).on('click', function(){
-    counter++;
-    $("#"+ catId).text(counter);
+    /*if(counter == 0){
+      self.display();
+    }*/
+    self.counter++;
+    $("#"+ self.catId).text(self.counter);
   });
-  this.counter = counter;
 }
